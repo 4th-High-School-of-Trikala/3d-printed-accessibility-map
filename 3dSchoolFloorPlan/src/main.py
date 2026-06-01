57e32767008cd1e8c39e2273b8faf47c48e35334
8wmas <thomas@onlinemail.gr>
2026-06-01 21:43:39 +0300

    Update main.py

--- 3dSchoolFloorPlan/src/main.py
+++ 3dSchoolFloorPlan/src/main.py
@@ -7,7 +7,7 @@ pygame.mixer.init(devicename="C-Media USB Headphone Set, USB Audio")
 sound=pygame.mixer.Sound("sound2.mp3")
 pl = 0
 
-def say_hello5() :
+def in24() :
     global sound
     global pl
     if pl != 1:
@@ -16,62 +16,56 @@ def say_hello5() :
         sound=pygame.mixer.Sound("nar_central1.mp3")
         sound.play(0)
         time.sleep(5)
-        print("Hello, Button 5!")
         pl = 0
     return
 
-def say_hello26():
+def in25():
+    global pl
+    global sound
+    if pl != 2:
+        pl = 2
+        sound.stop()
+        sound=pygame.mixer.Sound("nar_Wex.mp3")
+        sound.play(0)
+        time.sleep(5)
+        pl = 0
+    return
+
+def in26():
     global pl
     global sound
     if pl != 3:
         pl = 3
         sound.stop()
-        sound=pygame.mixer.Sound("nar_Wex.mp3")
+        sound=pygame.mixer.Sound("nar_central2.mp3")
         sound.play(0)
         time.sleep(5)
-        print("Hello, Button 26!")
         pl = 0
     return
 
-def say_hello16():
+def in27():
     global pl
     global sound
     if pl != 4:
         pl = 4
         sound.stop()
-        sound=pygame.mixer.Sound("nar_central2.mp3")
-        sound.play(0)
-        time.sleep(5)
-        print("Hello, Button 16!")
-        pl = 0
-    return
-
-def say_hello25():
-    global pl
-    global sound
-    if pl != 5:
-        pl = 5
-        sound.stop()
         sound=pygame.mixer.Sound("nar_Nex.mp3")
         sound.play(0)
         time.sleep(5)
-        print("Hello, Button 25!")
         pl = 0
     return
 
-button1 = Button(5, pull_up=True, hold_time=0.2)
-button2 = Button(6, pull_up=True, hold_time=0.2)
+button1 = Button(24, pull_up=True, hold_time=0.2)
+button2 = Button(25, pull_up=True, hold_time=0.2)
 button3 = Button(26, pull_up=True, hold_time=0.2)
-button4 = Button(16, pull_up=True, hold_time=0.2)
-button5 = Button(25, pull_up=True, hold_time=0.2)
+button4 = Button(27, pull_up=True, hold_time=0.2)
 
 
 
-button1.when_held = say_hello5
-button2.when_held = say_hello6
-button3.when_held = say_hello26
-button4.when_held = say_hello16
-button5.when_held = say_hello25
+button1.when_held = in24
+button2.when_held = in25
+button3.when_held = in26
+button4.when_held = in27
 
 
 pause()
