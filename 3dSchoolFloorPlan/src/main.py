68dbb01b23b429b92e30cd1f6667e7bd433bc7e6
8wmas <thomas@onlinemail.gr>
2026-06-02 11:29:47 +0300

    Update main.py

--- 3dSchoolFloorPlan/src/main.py
+++ 3dSchoolFloorPlan/src/main.py
@@ -7,9 +7,9 @@ pygame.mixer.init(devicename="C-Media USB Headphone Set, USB Audio")
 sound=pygame.mixer.Sound("sound2.mp3")
 pl = 0
 
-def in24() :
-    global sound
+def in23():
     global pl
+    global sound
     if pl != 1:
         pl = 1
         sound.stop()
@@ -19,11 +19,23 @@ def in24() :
         pl = 0
     return
 
+def in24() :
+    global sound
+    global pl
+    if pl != 2:
+        pl = 2
+        sound.stop()
+        sound=pygame.mixer.Sound("nar_Eex.mp3")
+        sound.play(0)
+        time.sleep(5)
+        pl = 0
+    return
+
 def in25():
     global pl
     global sound
-    if pl != 2:
-        pl = 2
+    if pl != 3:
+        pl = 3
         sound.stop()
         sound=pygame.mixer.Sound("nar_Wex.mp3")
         sound.play(0)
@@ -34,8 +46,8 @@ def in25():
 def in26():
     global pl
     global sound
-    if pl != 3:
-        pl = 3
+    if pl != 4:
+        pl = 4
         sound.stop()
         sound=pygame.mixer.Sound("nar_central2.mp3")
         sound.play(0)
@@ -46,8 +58,8 @@ def in26():
 def in27():
     global pl
     global sound
-    if pl != 4:
-        pl = 4
+    if pl != 5:
+        pl = 5
         sound.stop()
         sound=pygame.mixer.Sound("nar_Nex.mp3")
         sound.play(0)
@@ -55,17 +67,18 @@ def in27():
         pl = 0
     return
 
-button1 = Button(24, pull_up=True, hold_time=0.2)
-button2 = Button(25, pull_up=True, hold_time=0.2)
-button3 = Button(26, pull_up=True, hold_time=0.2)
-button4 = Button(27, pull_up=True, hold_time=0.2)
+button1 = Button(23, pull_up=True, hold_time=0.2)
+button2 = Button(24, pull_up=True, hold_time=0.2)
+button3 = Button(25, pull_up=True, hold_time=0.2)
+button4 = Button(26, pull_up=True, hold_time=0.2)
+button5 = Button(27, pull_up=True, hold_time=0.2)
 
 
-
-button1.when_held = in24
-button2.when_held = in25
-button3.when_held = in26
-button4.when_held = in27
+button1.when_held = in23
+button2.when_held = in24
+button3.when_held = in25
+button4.when_held = in26
+button5.when_held = in27
 
 
 pause()
